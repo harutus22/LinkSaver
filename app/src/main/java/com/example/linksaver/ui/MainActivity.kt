ff232fbc2eb7065e36d09ebce990fa773ba9c558
harutus22 <harutus22@yahoo.com>
2019-11-01 18:28:51 +0400

    Initial commit

---
+++ app/src/main/java/com/example/linksaver/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.linksaver.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.linksaver.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
